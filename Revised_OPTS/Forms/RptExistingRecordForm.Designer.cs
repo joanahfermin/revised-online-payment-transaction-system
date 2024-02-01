a77a6c03ec7ae5d12122a68311e7a02e934bd9f8
jfermin <ferminjoanah@gmail.com>
2024-02-01 13:27:40 +0800

    implementing 3 datagridview: rpt, business, misc in existingRecordForm

--- Revised_OPTS/Forms/RptExistingRecordForm.Designer.cs
+++ Revised_OPTS/Forms/RptExistingRecordForm.Designer.cs
@@ -32,14 +32,24 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnClose = new System.Windows.Forms.Button();
             this.btnSaveRecord = new System.Windows.Forms.Button();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.DgRptAddUpdateForm = new System.Windows.Forms.DataGridView();
+            this.DgBusAddUpdateForm = new System.Windows.Forms.DataGridView();
+            this.DgMiscAddUpdateForm = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.DgRptAddUpdateForm)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgBusAddUpdateForm)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgMiscAddUpdateForm)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -61,7 +71,7 @@
             this.btnClose.ForeColor = System.Drawing.Color.White;
             this.btnClose.Image = ((System.Drawing.Image)(resources.GetObject("btnClose.Image")));
             this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnClose.Location = new System.Drawing.Point(969, 34);
+            this.btnClose.Location = new System.Drawing.Point(1786, 30);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(126, 73);
             this.btnClose.TabIndex = 5;
@@ -79,7 +89,7 @@
             this.btnSaveRecord.ForeColor = System.Drawing.Color.White;
             this.btnSaveRecord.Image = ((System.Drawing.Image)(resources.GetObject("btnSaveRecord.Image")));
             this.btnSaveRecord.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSaveRecord.Location = new System.Drawing.Point(779, 34);
+            this.btnSaveRecord.Location = new System.Drawing.Point(1596, 30);
             this.btnSaveRecord.Name = "btnSaveRecord";
             this.btnSaveRecord.Size = new System.Drawing.Size(184, 73);
             this.btnSaveRecord.TabIndex = 6;
@@ -90,9 +100,9 @@
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(1101, 12);
+            this.pictureBox3.Location = new System.Drawing.Point(1336, 444);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(690, 782);
+            this.pictureBox3.Size = new System.Drawing.Size(576, 532);
             this.pictureBox3.TabIndex = 10;
             this.pictureBox3.TabStop = false;
             // 
@@ -117,7 +127,7 @@
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.DgRptAddUpdateForm.DefaultCellStyle = dataGridViewCellStyle2;
             this.DgRptAddUpdateForm.EnableHeadersVisualStyles = false;
-            this.DgRptAddUpdateForm.Location = new System.Drawing.Point(12, 143);
+            this.DgRptAddUpdateForm.Location = new System.Drawing.Point(12, 138);
             this.DgRptAddUpdateForm.Name = "DgRptAddUpdateForm";
             dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle3.BackColor = System.Drawing.Color.Salmon;
@@ -130,14 +140,90 @@
             this.DgRptAddUpdateForm.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.LightBlue;
             this.DgRptAddUpdateForm.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
             this.DgRptAddUpdateForm.RowTemplate.Height = 25;
-            this.DgRptAddUpdateForm.Size = new System.Drawing.Size(1083, 651);
+            this.DgRptAddUpdateForm.Size = new System.Drawing.Size(1900, 291);
             this.DgRptAddUpdateForm.TabIndex = 204;
+            // 
+            // DgBusAddUpdateForm
+            // 
+            this.DgBusAddUpdateForm.BackgroundColor = System.Drawing.Color.AliceBlue;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.Salmon;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.MistyRose;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.Desktop;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DgBusAddUpdateForm.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.DgBusAddUpdateForm.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DgBusAddUpdateForm.DefaultCellStyle = dataGridViewCellStyle5;
+            this.DgBusAddUpdateForm.EnableHeadersVisualStyles = false;
+            this.DgBusAddUpdateForm.Location = new System.Drawing.Point(12, 444);
+            this.DgBusAddUpdateForm.Name = "DgBusAddUpdateForm";
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.Salmon;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.MistyRose;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DgBusAddUpdateForm.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.DgBusAddUpdateForm.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.LightBlue;
+            this.DgBusAddUpdateForm.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.DgBusAddUpdateForm.RowTemplate.Height = 25;
+            this.DgBusAddUpdateForm.Size = new System.Drawing.Size(1307, 257);
+            this.DgBusAddUpdateForm.TabIndex = 204;
+            // 
+            // DgMiscAddUpdateForm
+            // 
+            this.DgMiscAddUpdateForm.BackgroundColor = System.Drawing.Color.AliceBlue;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.Salmon;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.MistyRose;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.Desktop;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DgMiscAddUpdateForm.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            this.DgMiscAddUpdateForm.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DgMiscAddUpdateForm.DefaultCellStyle = dataGridViewCellStyle8;
+            this.DgMiscAddUpdateForm.EnableHeadersVisualStyles = false;
+            this.DgMiscAddUpdateForm.Location = new System.Drawing.Point(12, 716);
+            this.DgMiscAddUpdateForm.Name = "DgMiscAddUpdateForm";
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle9.BackColor = System.Drawing.Color.Salmon;
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle9.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.Color.MistyRose;
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DgMiscAddUpdateForm.RowHeadersDefaultCellStyle = dataGridViewCellStyle9;
+            this.DgMiscAddUpdateForm.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.LightBlue;
+            this.DgMiscAddUpdateForm.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.DgMiscAddUpdateForm.RowTemplate.Height = 25;
+            this.DgMiscAddUpdateForm.Size = new System.Drawing.Size(1307, 260);
+            this.DgMiscAddUpdateForm.TabIndex = 204;
             // 
             // RptExistingRecordForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1803, 806);
+            this.ClientSize = new System.Drawing.Size(1924, 988);
+            this.Controls.Add(this.DgMiscAddUpdateForm);
+            this.Controls.Add(this.DgBusAddUpdateForm);
             this.Controls.Add(this.DgRptAddUpdateForm);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox3);
@@ -148,6 +234,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.DgRptAddUpdateForm)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgBusAddUpdateForm)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgMiscAddUpdateForm)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -159,5 +247,7 @@
         private Button btnSaveRecord;
         private PictureBox pictureBox3;
         private DataGridView DgRptAddUpdateForm;
+        private DataGridView DgBusAddUpdateForm;
+        private DataGridView DgMiscAddUpdateForm;
     }
 }
--- Revised_OPTS/Forms/RptExistingRecordForm.cs
+++ Revised_OPTS/Forms/RptExistingRecordForm.cs
@@ -22,12 +22,16 @@ namespace Inventory_System.Forms
         public RptExistingRecordForm(List<Rpt> existingRecordList)
         {
             InitializeComponent();
-            InitializeDataGridView();
+            InitializeRptDataGridView();
+            InitializeBusinessDataGridView();
+
             DgRptAddUpdateForm.DefaultCellStyle.Font = new Font("Tahoma", 12, FontStyle.Regular);
+            this.WindowState = FormWindowState.Maximized;
 
             DynamicGridContainer.PopulateData(existingRecordList);
         }
-        private void InitializeDataGridView()
+
+        private void InitializeRptDataGridView()
         {
             List<Bank> bankList = rptService.GetAllBanks();
             string[] bankNames = bankList.Select(bank => bank.BankName).ToList().ToArray();
@@ -37,16 +41,45 @@ namespace Inventory_System.Forms
                 new DynamicGridInfo{PropertyName="TaxDec", Label = "TDN", isRequired=true },
                 new DynamicGridInfo{PropertyName="TaxPayerName", Label = "TaxPayer's Name", isRequired=true },
                 new DynamicGridInfo{PropertyName="AmountToPay", Label = "Bill Amount", isRequired=true },
+                new DynamicGridInfo{PropertyName="TotalAmountTransferred", Label = "Total Amount Transferred", isRequired=true },
                 new DynamicGridInfo{PropertyName="Bank", Label = "Bank", GridType = DynamicGridType.ComboBox, ComboboxChoices = bankNames, isRequired=true },
                 new DynamicGridInfo{PropertyName="PaymentDate", Label = "Payment Date", GridType = DynamicGridType.DatetimePicker, isRequired=true },
-                new DynamicGridInfo{PropertyName="YearQuarter", Label = "Year", decimalValue = true},
+                new DynamicGridInfo{PropertyName="YearQuarter", Label = "Year", decimalValue = true, isRequired=true},
                 new DynamicGridInfo{PropertyName="Quarter", Label = "Quarter", GridType=DynamicGridType.ComboBox, ComboboxChoices = Quarter.ALL_QUARTER, isRequired=true },
+                new DynamicGridInfo{PropertyName="Status", Label = "Status", GridType=DynamicGridType.ComboBox, ComboboxChoices = TaxStatus.STATUS, isRequired=true },
                 new DynamicGridInfo{PropertyName="BillingSelection", Label = "Billing Selection", GridType=DynamicGridType.ComboBox, ComboboxChoices = BillingSelectionUtil.ALL_BILLING_SELECTION, isRequired=true },
                 new DynamicGridInfo{PropertyName="RequestingParty", Label = "Email Address" },
                 new DynamicGridInfo{PropertyName="RPTremarks", Label = "Remarks"},
             };
             DynamicGridContainer = new DynamicGridContainer<Rpt>(DgRptAddUpdateForm, gridInfoArray, true, true);
         }
+
+        private void InitializeBusinessDataGridView()
+        {
+            List<Bank> bankList = rptService.GetAllBanks();
+            string[] bankNames = bankList.Select(bank => bank.BankName).ToList().ToArray();
+
+            DynamicGridInfo[] gridInfoArray = new DynamicGridInfo[] {
+                new DynamicGridInfo{PropertyName="BusinessID", Label = "BusID", isReadOnly = true },
+                new DynamicGridInfo{PropertyName="Business_Type", Label = "Business Type", isRequired=true },
+                new DynamicGridInfo{PropertyName="MP_Number", Label = "MP Number", isRequired=true },
+                new DynamicGridInfo{PropertyName="TaxpayersName", Label = "TaxPayer's Name", isRequired=true },
+                new DynamicGridInfo{PropertyName="BusinessName", Label = "Business Name", isRequired=true },
+                new DynamicGridInfo{PropertyName="BillNumber", Label = "Bill Number", isRequired=true },
+                new DynamicGridInfo{PropertyName="AmountToPay", Label = "Bill Amount", isRequired=true },
+                new DynamicGridInfo{PropertyName="MiscFees", Label = "Misc. Fees", isRequired=true },
+                new DynamicGridInfo{PropertyName="TotalAmount", Label = "Total Amount Transferred", isRequired=true },
+                new DynamicGridInfo{PropertyName="PaymentChannel", Label = "Bank", GridType = DynamicGridType.ComboBox, ComboboxChoices = bankNames, isRequired=true },
+                new DynamicGridInfo{PropertyName="PaymentDate", Label = "Payment Date", GridType = DynamicGridType.DatetimePicker, isRequired=true },
+                new DynamicGridInfo{PropertyName="Year", Label = "Year", decimalValue = true},
+                new DynamicGridInfo{PropertyName="Qtrs", Label = "Quarter", GridType=DynamicGridType.ComboBox, ComboboxChoices = Quarter.ALL_QUARTER, isRequired=true },
+                new DynamicGridInfo{PropertyName="Status", Label = "Status", GridType=DynamicGridType.ComboBox, ComboboxChoices = TaxStatus.STATUS, isRequired=true },
+                new DynamicGridInfo{PropertyName="RequestingParty", Label = "Email Address" },
+                new DynamicGridInfo{PropertyName="BussinessRemarks", Label = "Remarks"},
+            };
+            DynamicGridContainer = new DynamicGridContainer<Rpt>(DgRptAddUpdateForm, gridInfoArray, true, true);
+        }
+
 
         private void btnClose_Click(object sender, EventArgs e)
         {
